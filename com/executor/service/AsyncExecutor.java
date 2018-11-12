79e290b19f8020a778d385aca4a2b1259f89bc2d
Nitesh Apte <niteshapte@users.noreply.github.com>
2018-11-13 03:48:58 +0530

    Create AsyncExecutor.java

---
+++ com/executor/service/AsyncExecutor.java
@@ -0,0 +1,58 @@
+package com.executor.service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * The Class AsyncExecutor.
+ */
+public class AsyncExecutor {
+	
+	/** The client. */
+	JerseyRestClient client = JerseyRestClient.getInstance();
+
+	/** The Constant urls. */
+	private final static String[] urls = {"https://jsonplaceholder.typicode.com/posts", "https://jsonplaceholder.typicode.com/comments", "https://jsonplaceholder.typicode.com/albums", "https://jsonplaceholder.typicode.com/photos", "https://jsonplaceholder.typicode.com/todos", "https://jsonplaceholder.typicode.com/users"};
+	
+	/**
+	 * Make async calls.
+	 */
+	public void makeAsyncCalls() {
+		List<String> listOfURLs = Arrays.asList(urls);
+		
+		ExecutorServiceProvider<Void> executorServiceProvider = new ExecutorServiceProvider<Void>();
+		ExecutorService service = executorServiceProvider.getExecutorService(5);
+		ExecutorCompletionService<Void> executorCompletionService = new ExecutorCompletionService<Void>(service);
+		
+		listOfURLs.forEach(url -> {
+			Callable<Void> worker = () -> {
+				callIndividualURLs(url);
+				return null;
+			};
+			try {
+				executorCompletionService.submit(worker);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+		executorServiceProvider.getResult(listOfURLs.size(), executorCompletionService, 5, TimeUnit.SECONDS);
+		executorServiceProvider.shutdownAndAwaitTermination(service);
+	}
+	
+	/**
+	 * Call individual UR ls.
+	 *
+	 * @param url the url
+	 * @return the string
+	 */
+	public String callIndividualURLs(String url) {
+		System.out.println(url);
+		String data = client.getResponseAsString(url);
+		System.out.println(data);
+		return data;
+	}
+}
