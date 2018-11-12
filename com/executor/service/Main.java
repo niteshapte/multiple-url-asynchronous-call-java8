a5e6e8cbbf0af7275177524df8f6e7080ec4a835
Nitesh Apte <niteshapte@users.noreply.github.com>
2018-11-13 03:49:28 +0530

    Create Main.java

---
+++ com/executor/service/Main.java
@@ -0,0 +1,9 @@
+package com.executor.service;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AsyncExecutor asyncExecutor = new AsyncExecutor();
+		asyncExecutor.makeAsyncCalls();
+	}
+}
